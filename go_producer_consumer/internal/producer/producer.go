b3dbf196354cb2e79897e932b26ddfc808c781b4
Dhruv Prajapati <dprajapati@juniper.net>
2025-05-21 18:28:25 +0530

    [UPDATED] Producer sarama conf

--- go_producer_consumer/internal/producer/producer.go
+++ go_producer_consumer/internal/producer/producer.go
@@ -21,10 +21,27 @@ type Producer struct {
 func NewProducer(cluster *config.ClusterConfig) (*Producer, error) {
 
 	saramaConfig := sarama.NewConfig()
-	saramaConfig.Producer.Return.Successes = true
 	saramaConfig.Producer.RequiredAcks = sarama.WaitForAll
 	saramaConfig.Producer.Retry.Max = 5
+	saramaConfig.Net.DialTimeout = 30 * time.Second
+	saramaConfig.Net.ReadTimeout = 30 * time.Second
+	saramaConfig.Net.WriteTimeout = 30 * time.Second
+	saramaConfig.Metadata.RefreshFrequency = 2 * time.Minute
+	saramaConfig.Metadata.Retry.Backoff = 2 * time.Second
+	saramaConfig.Producer.MaxMessageBytes = 1_000_000
+	saramaConfig.Producer.RequiredAcks = sarama.RequiredAcks(1)
+	saramaConfig.Producer.Timeout = 10 * time.Second
+	saramaConfig.Producer.Return.Successes = true
+	saramaConfig.Producer.Flush.Frequency = 5 * time.Second
+	saramaConfig.Producer.Flush.MaxMessages = 0
 	saramaConfig.Version, _ = sarama.ParseKafkaVersion(cluster.Version)
+
+	var err error
+	saramaConfig.Version, err = sarama.ParseKafkaVersion(cluster.Version)
+	if err != nil {
+		logger.Error("Failed to parse Kafka version", zap.String("func", utils.GetFunctionName(1)), zap.Error(err))
+		return nil, fmt.Errorf("invalid Kafka version: %w", err)
+	}
 
 	producer, err := sarama.NewSyncProducer(cluster.Brokers, saramaConfig)
 	if err != nil {
